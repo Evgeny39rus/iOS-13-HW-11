60182c1919a438ce27869b0fbe34b564bdf9de4e
Evgeny Sushkov <evgeny.sushkov@apollointernet.io>
2024-06-05 23:04:44 +0200

    new label display rules
    new label display rules

--- IOS-13-11/ViewController.swift
+++ IOS-13-11/ViewController.swift
@@ -15,15 +15,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 //        
-        Login.text = "Login"
-        Login.textAlignment = .center
-        Login.backgroundColor = .white // Фоновый цвет для лучшей видимости
-        Login.translatesAutoresizingMaskIntoConstraints = false
         
-        Login.layer.borderColor = UIColor.black.cgColor // Цвет обводки
-        Login.layer.borderWidth = 2.0 // Толщина обводки
-        Login.layer.cornerRadius = 10.0 // Радиус закругления углов (опционально)
-        Login.layer.masksToBounds = true // Обязателен для закругления углов
+        // Настройка текста с обводкой
+               let text = "Login"
+               let strokeTextAttributes: [NSAttributedString.Key: Any] = [
+                   .strokeColor: UIColor.black, // Цвет обводки
+                   .foregroundColor: UIColor.gray, // Цвет текста
+                   .strokeWidth: -1.0 // Толщина обводки (отрицательное значение для внутренней обводки)
+               ]
+        let attributedText = NSAttributedString(string: text, attributes: strokeTextAttributes)
+               
+               // Установка атрибутированного текста в UILabel
+               Login.attributedText = attributedText
+               
+               // Дополнительная настройка UILabel
+               Login.textAlignment = .center
+               Login.backgroundColor = .white // Фоновый цвет для лучшей видимости
+               Login.translatesAutoresizingMaskIntoConstraints = false
+//        Login.text = "Login"
+//        Login.textAlignment = .center
+//        Login.backgroundColor = .white // Фоновый цвет для лучшей видимости
+//        Login.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        Login.layer.borderColor = UIColor.black.cgColor // Цвет обводки
+//        Login.layer.borderWidth = 2.0 // Толщина обводки
+//        Login.layer.cornerRadius = 10.0 // Радиус закругления углов (опционально)
+//        Login.layer.masksToBounds = true // Обязателен для закругления углов
     }
 
  
